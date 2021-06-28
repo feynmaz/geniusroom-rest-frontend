cf8538c9eaa42b8e828b8f008a2a922c7bd3affc
feynmaz <feynmaz@gmail.com>
2021-06-28 15:48:17 +0500

    доработал стили

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,22 +6,28 @@ export const Menu = ({ changeStyleBlockArticles }) => {
     const [rubrics, setRubrics] = useState([])
     const history = useHistory()
     const menuList = useRef()
-    const menuArrow = useRef()
+    const menuOverlay = useRef()
 
     const changeStyleMenu = () => {
-        menuList.current.classList.toggle('active')
-        menuArrow.current.classList.toggle('active-arrow')
+        menuList.current.classList.add('active')
+        menuOverlay.current.classList.add('menu-overlay-active')
         changeStyleBlockArticles()
     }
 
     const openMenuList = () => changeStyleMenu()
 
     const selectRubric = async event => {
-        changeStyleMenu()
         history.push('/' + event.target.dataset.link)
+    }
+    
+    const closeMenu = () => {
+        changeStyleMenu()
+        menuList.current.classList.remove('active')
+        menuOverlay.current.classList.remove('menu-overlay-active')
     }
 
     useEffect(() => {
+
         async function fetchData() {
             const response = await fetch(`https://geniusroom-rest-backend.herokuapp.com/api/v1/articles/rubrics/`, {
                 headers : { 
@@ -33,17 +39,19 @@ export const Menu = ({ changeStyleBlockArticles }) => {
             setRubrics(data)
         }
         fetchData()
+        
       }, [])
 
     let superRubric = ''
 
     return (
+        <>
         <div className="menu">
-            <div className="menu-title-wrapper" onClick={openMenuList}>
-                <div ref={menuArrow} className="menu-title-arrow"></div>
-                <div className="menu-title">Menu</div>
+            <div className="menu-title-wrapper" onClick={openMenuList} title="Menu">
+                <svg className="menu-icon" height="384pt" viewBox="0 -53 384 384" width="384pt" xmlns="http://www.w3.org/2000/svg"><path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/></svg>
             </div>
-            <div ref={menuList} className="menu-items">
+            <div ref={menuList} className="menu-items-wrapper" onClick={e => e.stopPropagation()}>
+            <div className="menu-items">
                 {
                     rubrics.map((rubric, index) => {
                         let element = (<></>)
@@ -69,6 +77,9 @@ export const Menu = ({ changeStyleBlockArticles }) => {
                     })
                 }
             </div>
+            </div>
         </div>
+        <div ref={menuOverlay} onClick={closeMenu} className="menu-overlay"></div>
+        </>
     )
 }
--- src/context/RatingContext.js
+++ src/context/RatingContext.js
@@ -1,6 +1,4 @@
-import { createContext } from "react";
-import { useContext } from "react";
-import { React } from 'react'
+import { createContext } from "react"
 
 export const RatingContext = createContext({
     rating: null,
